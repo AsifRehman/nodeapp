b3b0c00d80abb235f68563a6cbc6749ca9e33d0a
Asif Rehman <asifrehman007@hotmail.com>
2022-04-16 18:13:43 +0500

    Ledger feature Added

---
+++ routes/ledger.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+
+const CR = require("../models/cr");
+const CP = require("../models/cp");
+const JV = require("../models/jv");
+const COMP = require("../models/company");
+
+router.get('/test', async (req, res) => {
+    const tbl = await JV.find().exec()
+    res.json(tbl)
+})
+
+router.get('/:id', async (req, res) => {
+
+    let comp = await COMP.findOne({ companyId: 1 }).exec();
+
+    let tbl = await JV.findOne({ jvNum: req.params.id }).exec();
+    if (tbl != null) {
+        const title = "Journal Voucher"
+        res.render("printv", { layout: false, jvs: tbl, title: title, comp: comp })
+        return;
+    }
+
+    tbl = await CR.findOne({ jvNum: req.params.id }).exec();
+    if (tbl != null) {
+        const title = "Cash Receipt"
+        res.render("printv", { layout: false, jvs: tbl, title: title, comp: comp })
+        return;
+    }
+
+    tbl = await CP.findOne({ jvNum: req.params.id }).exec();
+    if (tbl != null) {
+        const title = "Cash Payment"
+        res.render("printv", { layout: false, jvs: tbl, title: title, comp: comp })
+        return;
+    }
+
+})
+
+
+module.exports = router
